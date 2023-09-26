b7811e870d0ad07bd8ef918f9c21df1827b13772
TechRH1 <74297257+TechRH1@users.noreply.github.com>
2023-09-26 12:17:16 +0200

    rounded corners + verbreding

--- productoverzichtspagina.php
+++ productoverzichtspagina.php
@@ -20,25 +20,25 @@
     <center>
         <section style="background-color: RGB(173, 216, 230);">
             <h2 style="color: black;">Alle Producten</h2>
-                <div style=" display: inline-block; margin: 20px; width: 200px; border: 1px solid black; padding: 10px; background-color: white;">
-                    <img src="images/product1.png" style="width: 120px; height: 100px; object-fit:contain">
+                <div style=" display: inline-block; margin: 20px; width: 300px; border: 1px solid black; padding: 10px; border-radius: 10px; background-color: white;">
+                    <img src="images/product1.png" style="width: 260px; height: 100px; object-fit:contain">
                     <h3 style="color: black;">Samsung A53</h3>
                     <p style="color: black;">De A53 is een geadvanceerde telefoon door Samsung geproduceerd</p>
-                    <a href="Samsung-A53.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px black;">Meer info</a>
+                    <a href="Samsung-A53.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black;">Meer info</a>
                 </div>
 
-                <div style=" display: inline-block; margin: 20px; width: 200px; border: 1px solid black; padding: 10px; background-color: white;">
-                    <img src="images/product2copy.png" style="width: 120px; height: 100px; object-fit:contain" />
+                <div style=" display: inline-block; margin: 20px; width: 300px; border: 1px solid black; padding: 10px; border-radius: 10px; background-color: white;">
+                    <img src="images/product2copy.png" style="width: 260px; height: 100px; object-fit:contain" />
                     <h3 style="color: black;">Flipper Zero</h3>
                     <p style="color: black;">Flipper Zero is een draagbare multitool voor pentesters en geeks in een speelgoedachtig jasje.</p>
-                    <a href="product2.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px black;">Meer info</a>
+                    <a href="product2.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black;">Meer info</a>
                 </div>
 
-                <div style=" display: inline-block; margin: 20px; width: 200px; border: 1px solid black; padding: 10px; background-color: white;">
-                    <img src="images/product1.png" style="width: 120px; height: 100px; object-fit:contain">
+                <div style=" display: inline-block; margin: 20px; width: 300px; border: 1px solid black; padding: 10px; border-radius: 10px; background-color: white;">
+                    <img src="images/product1.png" style="width: 260px; height: 100px; object-fit:contain">
                     <h3 style="color: black;">Samsung A53</h3>
                     <p style="color: black;">De A53 is een geadvanceerde telefoon door Samsung geproduceerd</p>
-                    <a href="product3.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px black;">Meer info</a>
+                    <a href="product3.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black;">Meer info</a>
         </section>
     </center>
 
