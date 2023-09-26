ed09bc431256aedf3b1f07ad5bda3cb3febde24b
yoery <81920433+yoery@users.noreply.github.com>
2023-09-26 10:23:57 +0200

    Image folder

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
     ?>
     <section style="width: auto; height: 800; position: center;">
         <center>
-                <img src="nerdy_gadgets.png" style="width: 800px; height: 260px; margin-top: 50px;">   
+                <img src="images/nerdy_gadgets.png" style="width: 800px; height: 260px; margin-top: 50px;">   
                 <h4 style="color: black; margin-top: 10px;">Smart Tech for Nerdy Minds</h4>
         </center>
     </section>
 
     <center>
         <section style="width: 900px; height: 500px; background-color: RGB(173, 216, 230); margin-top: 50px;">
-        <img src="infopic.png" style="width: 400px; height: 200px; margin-top: 30px;">
+        <img src="images/infopic.png" style="width: 400px; height: 200px; margin-top: 30px;">
         <p style="color: black; margin-top: 20px; width: 700px;">De missie van Nerdy Gadgets is om innovatieve technologieën en slimme gadgets toegankelijk te maken voor iedereen, ongeacht hun niveau van technische kennis. 
             Ons merk streeft ernaar om de nieuwsgierigheid en passie voor technologie te stimuleren, terwijl we de brug slaan tussen geeks en gadget-enthousiastelingen. 
             We geloven in de kracht van technologie om levens te verbeteren en streven ernaar om hoogwaardige, praktische, en soms speelse gadgets te bieden die het dagelijkse leven verrijken. 
@@ -32,25 +32,25 @@
         <section style="width: 900px; height: 750px; background-color: RGB(173, 216, 230);">
         <h2 style="color: black; margin-bottom: 20px;">Winkelervaring bij Nerdy Gadgets</h2>
         <div>
-            <img src="klantenrecensies.png" style="width: 75px; height: 75px;">
+            <img src="images/klantenrecensies.png" style="width: 75px; height: 75px;">
             <h3 style="color: black;">Klantrecensies</h3>
             <p style="color: black;">Ontdek wat onze klanten te zeggen hebben over hun Nerdy Gadgets-ervaring.</p>
         </div>
 
         <div>
-            <img src="navigatie.png" style="width: 75px; height: 75px;">
+            <img src="images/navigatie.png" style="width: 75px; height: 75px;">
             <h3 style="color: black;">Eenvoudige Navigatie</h3>
             <p style="color: black;">Vind gemakkelijk de gadgets die je zoekt met onze intuïtieve navigatie.</p>
         </div>
 
         <div>
-            <img src="betaling.png" style="width: 75px; height: 75px;">
+            <img src="images/betaling.png" style="width: 75px; height: 75px;">
             <h3 style="color: black;">Veilige Betalingsmogelijkheden</h3>
             <p style="color: black;">Betaal met vertrouwen dankzij onze veilige betalingsmethoden.</p>
         </div>
 
         <div>
-            <img src="snelle-levering.png" style="width: 75px; height: 75px;">
+            <img src="images/snelle-levering.png" style="width: 75px; height: 75px;">
             <h3 style="color: black;">Snelle Levering</h3>
             <p style="color: black;">Ontvang je bestelling snel en efficiënt, zodat je snel kunt genieten van je gadgets.</p>
         </div>
@@ -62,7 +62,7 @@
         <section style="background-color: RGB(173, 216, 230);">
             <h2 style="color: black;">Aanbevolen Producten</h2>
                 <div style=" display: inline-block; margin: 20px; width: 200px; border: 1px solid black; padding: 10px; background-color: white;">
-                    <img src="product1.png" style="width: 80px; height: 100px;">
+                    <img src="images/product1.png" style="width: 80px; height: 100px;">
                     <h3 style="color: black;">Samsung A53</h3>
                     <p style="color: black;">De A53 is een geadvanceerde telefoon door Samsung geproduceerd</p>
                     <a href="product1.php" style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px black;">Meer info</a>
