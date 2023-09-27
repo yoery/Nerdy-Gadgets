96179f9497f556c110a32feecf6126e10b281e3f
FireKnight76 <fireknight593@gmail.com>
2023-09-27 15:40:20 +0200

    search bar
    design van een zoekfunctie, voor nu zonder functionaliteit

--- productoverzichtspagina.php
+++ productoverzichtspagina.php
@@ -5,11 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productoverzicht</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="productoverzichtspagina.css">
 </head>
 <body class="body">
     <?php
     include 'navigation.php';
     ?>
+    <section>
+        <div class="search_bar_location">
+            <form action="" class="search_bar">
+                <input type="text" placeholder="search anything" name="q">
+                <button type="submit"><img src="images/search.png"></button>
+            </form>
+        </div>
+    </section>
+
     <section style="width: auto; height: 800; position: center;">
         <center>
                 <img src="images/nerdy_gadgets.png" style="width: 800px; height: 260px; margin-top: 50px;">   
