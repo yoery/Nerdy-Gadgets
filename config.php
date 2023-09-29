dd18165d8962e1b008757b26928326ba15a6bbec
Jim-Wetering <145760054+Jim-Wetering@users.noreply.github.com>
2023-09-29 13:08:44 +0200

    config file aangepast

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=voedselbank_db", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=`nerdygadgets.sql`", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
