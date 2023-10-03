234486da9f47638b947b9418ea8ae017b4ba5d7e
RCardose13 <raylian.cardose@hva.nl>
2023-10-03 14:29:59 +0200

    Het A53 pagina
    Opnieuw gepushed het hele pagina van het a53 klaar gemaakt. ik heb plus/min punten. een kleine beschrijving van het product.

--- Samsung-A53.php
+++ Samsung-A53.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,11 +10,40 @@
     <?php
     include 'navigation.php';
     ?>
-    <section style="width: auto; height: 800; position: center;">
+    <section style="width: 100; height: 700; position: center;">
         <center>
-                <img src="images/nerdy_gadgets.png" style="width: 800px; height: 260px; margin-top: 50px;">   
+                <img src="images/nerdy_gadgets.png" style="width: 450px; height: 100px; margin-top: 30px;">
                 <h4 style="color: black; margin-top: 10px;">Smart Tech for Nerdy Minds</h4>
         </center>
     </section>
+    <center>
+        <section style="width: 800px; height: 500px; margin-top: 50px; border-radius: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+            <img src="images/1715465.jpg" style="width: 150px; height: 250px; margin-top: 30px; border-radius: 10px;">
+            <img src="images/1857490.jpg" style="width: 150px; height: 250px; margin-top: 30px; border-radius: 10px;">
+            <img src="images/1748730.jpg" style="width: 200px; height: 250px; margin-top: 30px; border-radius: 10px;">
+            <h5 style="color: black; margin-top: 20px; width: 700px;">De Galaxy A-serie heeft er een nieuwe ster bij: de Galaxy A53 5G Zwart.
+                Deze waterbestendige 128GB-smartphone valt op door zijn kleurrijke, antireflecterende 6.6-inch-Infinity-V Display en het minimalistische design met een afwerking van glas.
+                De grote 5.000mAh-batterij gaat dagenlang mee, zodat je genoeg tijd hebt om prachtige beelden vast te leggen met het krachtige camerasysteem dat over optische beeldstabilisatie beschikt.</h5>
+        </section>
+    </center>
+    <center>
+        <section>
+            <h2 style="color: black; margin-bottom: 20px;">Samsung A53 Klantrecensies</h2>
+            <div style=" display: inline-block; margin: 20px; width: 350px; height: 250px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 10px; padding: 10px; background-color: white;">
+                <img src="images/klantenrecensies.png" style="width: 75px; height: 75px;">
+                <h3 style="color: black;">Klantrecensies</h3>
+                <p style="color: black;">Ontdek wat onze klanten te zeggen hebben over het samsung A53.</p>
+            </div>
+        </section>
+        <section></section>
+            <div style=" display: inline-block; margin: 20px; width: 350px; height: 450px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 10px; padding: 10px; background-color: white;">
+                <h2 style="color: black; margin-bottom: 20px;"> Plus/Min punten</h2>
+                <img src="images/Galaxy-A-Unpacked_press-release_main1.jpg" style="width: 75px; height: 75px;">
+                <h3 style="color: black;">Productinformatie</h3>
+                <p style="color: black;"><b>Plus:</b> Je laadt je nieuwe A53 in iets meer dan uur helemaal op met de meegeleverde oplader.</p>
+                <p style="color: black;"><b>Plus:</b> Met de grote 5.000 mAh batterij raakt je smartphone niet snel leeg.</p>
+                <p style="color: black;"><b>Plus:</b> Je bekijkt films en series in hoge kwaliteit op het scherpe full hd scherm.</p>
+                <p style="color: black;"><b>Min:</b>  Je bedient het grote 6,5 inch scherm moeilijk met één hand.</p>
+            </div>
  </body>
 </html>
