c5a58edce38ff482d2ac39e33b9c07cf23f5773f
FireKnight76 <fireknight593@gmail.com>
2023-10-13 11:08:11 +0200

    gallary slideshow start

--- Samsung-A53.php
+++ Samsung-A53.php
@@ -31,20 +31,18 @@
                 De grote 5.000mAh-batterij gaat dagenlang mee, zodat je genoeg tijd hebt om prachtige beelden vast te leggen met het krachtige camerasysteem dat over optische beeldstabilisatie beschikt.</h5>
         </section>
     </center>
-    
+
     <section>
         <div class="samsung-container">
 
             <div class="mySlides">
                 <div class="numbertext">1 / 3</div>
                 <img src="images/samsung1.png">
-                <div class="text">Caption 1</div>
             </div>
 
             <div class="mySlides">
                 <div class="numbertext">2 / 3</div>
                 <img src="images/samsung%20side2.png">
-                <div class="text">Caption 2</div>
             </div>
 
             <div class="mySlides">
@@ -52,18 +50,67 @@
                 <img src="images/samsung%20side1.png">
             </div>
 
-            <a class="prev" onclick="plusSlides(-1)"></a>
-            <a class="next" onclick="plusSlides(+1)"></a>
+            <a class="prev" onclick="plusSlides(-1)">>&#10094;</a>
+            <a class="next" onclick="plusSlides(+1)">>&#10095;</a>
+
+            <div class="caption-container">
+                <p id="caption"></p>
+            </div>
+
+            <div class="row">
+                <div class="column">
+                    <img class="demo cursor" src="images/samsung1.png" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="images/samsung%20side1.png" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="images/samsung%20side2.png" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                </div>
+            </div>
         </div>
-        <br>
+
+        <script>
+            let slideIndex = 1;
+            showSlides(slideIndex);
+
+            // Next/previous controls
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+
+            // Thumbnail image controls
+            function currentSlide(n) {
+                showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+                let i;
+                let slides = document.getElementsByClassName("mySlides");
+                let dots = document.getElementsByClassName("demo");
+                let captionText = document.getElementById("caption");
+                if (n > slides.length) {slideIndex = 1}
+                if (n < 1) {slideIndex = slides.length}
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex-1].style.display = "block";
+                dots[slideIndex-1].className += " active";
+                captionText.innerHTML = dots[slideIndex-1].alt;
+            }
+        </script>
 
         <div style="text-align: center">
             <span class="dot" onclick="currentSlide(1)"></span>
             <span class="dot" onclick="currentSlide(2)"></span>
             <span class="dot" onclick="currentSlide(3)"></span>
         </div>
-
     </section>
+
+
     <center>
         <section>
             <h2 style="color: black; margin-bottom: 20px;">Samsung A53 Klantrecensies</h2>
