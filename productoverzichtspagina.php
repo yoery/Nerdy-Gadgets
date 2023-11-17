00f9cbbc81c26ea7e7dfeff6dd6287190d65216a
yoery <81920433+yoery@users.noreply.github.com>
2023-11-17 12:24:46 +0100

    Eindelijk werkt het

--- productoverzichtspagina.php
+++ productoverzichtspagina.php
@@ -7,6 +7,19 @@
     <title>Productoverzicht</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <style>
+        .section-logo {width: auto; height: 800; position: center;}
+        .img-logo {width: 800px; height: 260px; margin-top: 50px; border-radius: 10px;}
+        .h4-logo {color: black; margin-top: 10px;}
+
+        .producten {width: 1300px; height: 450px; margin-top: 20px; border-radius: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}
+        .product-info {display: inline-block; margin: 20px; height: 350px; width: 350px; padding: 10px; border-radius: 10px; background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}
+        .img-producten {width: 260px; height: 100px; object-fit:contain; padding: 5px;}
+        .h3-producten {color: black;}
+        .p-producten {color: black;}
+        .h2-producten {color: black; padding: 5px;}
+        .link-producten{ display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black; margin-top: 1px;}
+    </style>
 </head>
 
 
@@ -16,17 +29,46 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "nerdygadgets";
-    $mysqli = mysqli_connect($servername, $username, $password, $database) or die("Kan niet verbinden '$servername'");
-    mysqli_select_db($mysqli, $database) or die("Kon niet verbinden met '$database'");
+    $database = "nerdy_gadgets_start";
+    $conn = new mysqli($servername, $username, $password, $database);
+    
+    // Controleren op verbinding
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    // Query om gegevens op te halen
+    $sql = "SELECT name, description, price, image FROM product";
+    $result = $conn->query($sql);
+
+    // Controleren op resultaten
+    if ($result->num_rows > 0) {
+        // Resultaten verwerken in een for each loop
+        while ($row = $result->fetch_assoc()) {
+            // Beschrijving inkorten tot 50 tekens
+            $shortDescription = substr($row["description"], 0, 150);
+            $shortName = substr($row["name"], 0, 17);
+
+            echo "<div class='product-info'>";
+            echo "<img src='" . $row["image"] . "' alt='Productafbeelding' class='img-producten'>";
+            echo "<h2 class='h3-producten'>" . $shortName . " <a href='#'>...</a></h2>";
+            echo "<p class='p-producten'>" . $shortDescription . " <a href='#'>Lees meer</a></p>";
+            echo "<p class='h2-producten'>Price: $" . $row["price"] . "</p>";
+            echo "<button type='button' class='link-producten'>Voeg toe aan winkelwagen</button>";
+            echo "</div>";
+        }
+    } else {
+        echo "Geen resultaten gevonden";
+    }
+
+    $conn->close();
     ?>
 
 
-    <section style="width: auto; height: 800; position: center;">
+    <section class="section-logo">
         <center>
-            <img src="images/nerdy_gadgets.png"
-                style="width: 800px; height: 260px; margin-top: 50px; border-radius: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-            <h4 style="color: black; margin-top: 10px;">Smart Tech for Nerdy Minds</h4>
+            <img src="images/nerdy_gadgets.png" class="img-logo">
+            <h4 class="h4-logo">Smart Tech for Nerdy Minds</h4>
         </center>
     </section>
 
@@ -38,61 +80,15 @@
             <button class="btn" onclick="filterProducts('hacking')">Hacking</button>
             <button class="btn" onclick="filterProducts('vr')">Virtual Reality</button>
         </div>
-        <section id="producten"
-            style="width: 1300px; height: 450px; margin-top: 20px; border-radius: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-            <div class="product hacking">
-                <div
-                    style=" display: inline-block; margin: 20px; height: 350px; width: 350px; padding: 10px; border-radius: 10px; background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                    <img src="images/product2copy.png"
-                        style="width: 260px; height: 100px; object-fit:contain; padding: 5px;" />
-                    <h3 style="color: black;">Flipper Zero</h3>
-                    <p style="color: black;">Flipper Zero is een draagbare multitool voor pentesters en geeks in een
+        <section class="producten">
+            <div class="product-info">
+                    <img src="images/product2copy.png" class="img-producten">
+                    <h3 class="h3-producten">Flipper Zero</h3>
+                    <p class="p-producten">Flipper Zero is een draagbare multitool voor pentesters en geeks in een
                         speelgoedachtig jasje.</p>
-                    <h2 style="color: black; padding: 5px;">€250,-</h2>
-                    <a href="product2.php"
-                        style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black; margin-top: 1px;">Meer
-                        info</a>
+                    <h2 class="h2-producten">€250,-</h2>
+                    <a href="#" class="link-producten">Meer info</a>
                 </div>
-            </div>
-            <div class="product hacking">
-                <div
-                    style=" display: inline-block; margin: 20px; height: 350px; width: 350px; padding: 10px; border-radius: 10px; background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                    <img src="images/product2copy.png"
-                        style="width: 260px; height: 100px; object-fit:contain; padding: 5px;" />
-                    <h3 style="color: black;">
-                        <?php
-                        $result = mysqli_query($mysqli,"SELECT Merk, Productnaam FROM producten WHERE Merk = 'Flipper'");
-                        while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
-                            printf("%s %s <br>", $row[0], $row[1]);
-                        }
-                        ?></h3>
-                    <p style="color: black;"><?php
-                        $result = mysqli_query($mysqli,"SELECT Description FROM producten WHERE Merk = 'Flipper'");
-                        while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
-                            printf("%s <br>", $row[0]);
-                        }
-                        ?></p>
-                    <h2 style="color: black; padding: 5px;">€250,-</h2>
-                    <a href="product2.php"
-                        style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black; margin-top: 1px;">Meer
-                        info</a>
-                </div>
-            </div>
-            <div class="product vr">
-                <div
-                    style=" display: inline-block; margin: 20px; height: 350px; width: 350px; padding: 10px; border-radius: 10px; background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                    <img src="images/product3.png"
-                        style="width: 260px; height: 100px; object-fit:contain; padding: 5px;">
-                    <h3 style="color: black;">Meta Quest Pro</Prog>
-                    </h3>
-                    <p style="color: black;">De Meta Quest Pro is een mixed reality-headset ontwikkeld door Reality
-                        Labs, een divisie van Meta Platforms.</p>
-                    <h2 style="color: black; padding: 5px;">€1.199,-</h2>
-                    <a href="product3.php"
-                        style="  display: block; text-align: center; background-color: #007bff; color: black; text-decoration: none; padding: 5px; border-radius: 5px; black; margin-top: 1px;">Meer
-                        info</a>
-                </div>
-            </div>
         </section>
     </center>
 
