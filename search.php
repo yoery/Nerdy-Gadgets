fc8721f62035a94a225a0c8e8f2b473aa95f26c8
FireKnight76 <fireknight593@gmail.com>
2023-11-23 13:58:09 +0100

    scuffed search

--- search.php
+++ search.php
@@ -3,15 +3,40 @@
 <head>
 
 </head>
+<style>
+    table,th,tr{
+        border: 1px solid black;
+    }
+</style>
 <body>
 <form action="searchpage.php?searching=true" method="POST">
     <input type="text" name="searchcategory" value="">
     <input type="submit" value="search">
 </form>
 
+
 </body>
 
 
+<table>
+    <thead>
+    <tr>
+        <th>
+            name
+        </th>
+        <th> desciption</th>
+        <th>price</th>
+        <th>image</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>
+
+        </td>
+    </tr>
+    </tbody>
+</table>
 </html>
 
 <?php
--- searchpage.php
+++ searchpage.php
@@ -1,7 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
 
-
+</head>
+<style>
+    table,th,tr{
+        border: 1px solid black;
+    }
+</style>
 
 <table>
+
+
+    <thead>
+    <tr>
+        <th>
+            name
+        </th>
+        <th> desciption</th>
+        <th>price</th>
+        <th>image</th>
+    </tr>
+    </thead>
+
+    <tbody>
     <?php
 
     $servername = "localhost";
@@ -16,13 +38,43 @@
     } catch (PDOException $e) {
         die("Connection failed: " . $e->getMessage());
     }
+//    $sql=$conn->prepare('SELECT name, description, price, image FROM product WHERE name=:category');
+//    $sql->execute(array(':category'=>$_REQUEST['searchcategory']));
+//
+//    while($row=$sql->fetch()){
+//        echo "<tr><td>$row[name]</td><td>$row[price]</td><td>$row[category]</td><tr/>";
+//    }
+    if (isset($_POST['searchcategory'])){
 
-    $sql=$conn->prepare('SELECT * FROM product WHERE name=:category');
-    $sql->execute(array(':category'=>$_REQUEST['searchcategory']));
+        $connection = mysqli_connect("localhost", "root", "", "nerdy_gadgets_start");
+        $filtervalue = $_POST['searchcategory'];
+        $filterdata = "SELECT * FROM product WHERE CONCAT(name,description,price,image) LIKE '%$filtervalue%'";
+        $filterdata_run = mysqli_query($connection, $filterdata);
 
-    while($row=$sql->fetch()){
-        echo "<tr><td>$row[name]</td><td>$row[price]</td><td>$row[category]</td><tr/>";
+        if (mysqli_num_rows($filterdata_run) > 0)
+        {
+            foreach ($filterdata_run as $data)
+            {
+                ?>
+                <tr>
+                    <td><?php echo $data['name']?></td>
+                    <td><?php echo $data['description']?></td>
+                    <td><?php echo $data['price']?></td>
+                    <td><?php echo $data['image']?></td>
+                </tr>
+                <?php
+            }
+        }
+        else{
+            echo"<tr><td colspan='4'>no results</td></tr>";
+        }
     }
     ?>
+    <tr>
+        <td>
+
+        </td>
+    </tr>
+    </tbody>
 </table>
 
