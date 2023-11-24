ae1a8151cef63e7eb5926a9a35c032996a313c8c
FireKnight76 <fireknight593@gmail.com>
2023-11-24 13:25:19 +0100

    search en prdpagina gesplitst

--- productoverzichtspagina.php
+++ productoverzichtspagina.php
@@ -60,65 +60,7 @@
                 <option value="opslag">Opslag</option>
             </select>
         </div>
-        <table style="max-width: 100%"; border="solid black">
-            <tbody>
-            <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $database = "nerdy_gadgets_start";
-            try {
-                $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-                // Set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                die("Connection failed: " . $e->getMessage());
-            }
-            //    $sql=$conn->prepare('SELECT name, description, price, image FROM product WHERE name=:category');
-            //    $sql->execute(array(':category'=>$_REQUEST['searchcategory']));
-            //
-            //    while($row=$sql->fetch()){
-            //        echo "<tr><td>$row[name]</td><td>$row[price]</td><td>$row[category]</td><tr/>";
-            //    }
-            if (isset($_POST['searchcategory'])){
-                $connection = mysqli_connect("localhost", "root", "", "nerdy_gadgets_start");
-                $filtervalue = $_POST['searchcategory'];
-                $filterdata = "SELECT * FROM product WHERE lower(name) LIKE LOWER('%$filtervalue%')";
-                $filterdata_run = mysqli_query($connection, $filterdata);
-                $imagePath = '/Nerdy-Gadgets/product_images/';
-                if (mysqli_num_rows($filterdata_run) > 0)
-                {
-                    ?>
-                    <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>desciption</th>
-                        <th>price</th>
-                        <th>image</th>
-                    </tr>
-                    </thead>
-                    <?php
-                    foreach ($filterdata_run as $data)
-                    {
-                        $image = $data['image'];
-                        ?>
-                        <tr>
-                            <td><?php echo $data['name']?></td>
-                            <td><?php echo substr($data['description'], 0, 251)."..."?><a href="">lees meer</a></td>
-                            <td><?php echo $data['price']?></td>
-                            <td><?php echo "<img src=$imagePath/$image.jpg style='width: 50%'>"?></td>
-                            <td><?php echo "<button type='button' class='link-producten2' onclick='redirectToProduct(\"{$data["name"]}\")'> More info</button>"; ?></td>
-                        </tr>
-                        <?php
-                    }
-                }
-                else{
-                    echo"<tr><td colspan='4'>no results</td></tr>";
-                }
-            }
-            ?>
-            </tbody>
-        </table>
+
         <section class="producten">
             <?php
             $servername = "localhost";
